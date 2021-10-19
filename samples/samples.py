3abdebb1fdeee5d8f7ca8b912758aa7bcd4e90a0
nahanaoo <ericsven.u@gmail.com>
2021-10-19 19:21:36 +0200

    shrinked samples class

--- samples/samples.py
+++ samples/samples.py
@@ -16,36 +16,33 @@ class Samples():
         self.abbreviations['Agrobacterium tumefaciens'] = 'at'
         self.abbreviations['Microbacterium saperdae'] = 'ms'
         self.abbreviations['Ochrobactrum anthropi'] = 'oa'
-
         self.abbreviations['ct'] = 'Comamonas testosteroni'
         self.abbreviations['at'] = 'Agrobacterium tumefaciens'
         self.abbreviations['ms'] = 'Microbacterium saperdae'
         self.abbreviations['oa'] = 'Ochrobactrum anthropi'
-
-        #Adding reference fastas to class
-        self.references = dict()
-        self.references['Comamonas testosteroni'] = '/users/eulrich/evomicrocomm/references/ct/ct.fasta'
-        self.references['Agrobacterium tumefaciens'] = '/users/eulrich/evomicrocomm/references/at/at.fasta'
-        self.references['Microbacterium saperdae'] = '/users/eulrich/evomicrocomm/references/ms/ms.fasta'
-        self.references['Ochrobactrum anthropi'] = '/users/eulrich/evomicrocomm/references/oa/oa.fasta'
       
         """For every strain and treatment we create a  list of dictionaries with
         the information about directory, sample name, treatment and sequencing platform"""
-        self.strains = dict()
-        self.treatments = {1:[],2:[],3:[],4:[]}
-        strains = set(self.df['strain'])
-        for strain in strains:
-            self.strains[strain] = []
-            df = self.df[self.df['strain'] == strain]
-            for i,row in df.iterrows():
-                meta = dict()
-                meta['dir'] = row.dir_name
-                meta['name'] = row.sample_name
-                meta['strain'] = strain
-                if row.platform == 'pacbio':
-                    meta['treatment'] = int(row.sample_name[2])
-                if row.platform == 'illumina':
-                    meta['treatment'] = int(row.sample_name[4])
-                meta['platform'] = row.platform
-                self.strains[strain].append(meta)
-                self.treatments[meta['treatment']].append(meta)
+        keys = ['dir_name','name','strain','platform','timepoint','treatment','cosm']
+        meta = {key:None for key in keys}
+
+        keys = ['Agrobacterium tumefaciens','Comamonas testosteroni',\
+            'Microbacterium saperdae','Ochrobactrum anthropi']
+        self.strains = {k:list() for k in keys}
+
+        keys = [1,2,3,4]
+        self.treatments = {k:list() for k in keys}        
+        self.samples = {name:meta for name in self.df['sample_name']}
+
+        for i,row in self.df.iterrows():
+            meta['dir_name'] = row['dir_name']
+            meta['name'] = row['sample_name']
+            meta['strain'] = row['strain']
+            meta['platform'] = row['platform']
+            meta['timepoint'] = row['timepoint']
+            meta['treatment'] = row['treatment']
+            meta['cosm'] = row['cosm']
+            
+            self.samples[row['sample_name']] = meta
+            self.treatments[int(row['treatment'])].append(meta)
+            self.strains[row['strain']].append(meta)
