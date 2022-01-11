c432ac24c56a0612f354600f2f4c21791e3ba039
nahanoo <eric.ulrich@unil.ch>
2022-01-11 09:46:02 +0100

    added strains per treatment

--- samples/samples.py
+++ samples/samples.py
@@ -3,51 +3,86 @@ import pandas as pd
 import pkg_resources
 
 
-class Samples():
+class Samples:
     """This is a class used for parsing. It combines samples
     of Illumina and Pacbio"""
+
     def __init__(self):
-        self.sample_sheet = pkg_resources.resource_filename('samples','sample_sheet.csv')
-        self.df = pd.read_csv(self.sample_sheet,dtype=str)
-        
-        self.work = '/work/FAC/FBM/DMF/smitri/evomicrocomm/genome_size/data/'
+        self.sample_sheet = pkg_resources.resource_filename(
+            "samples", "sample_sheet.csv"
+        )
+        self.df = pd.read_csv(self.sample_sheet, dtype=str)
 
-        #Adding abbreviations for convenience
+        self.work = "/work/FAC/FBM/DMF/smitri/evomicrocomm/genome_size/data/"
+
+        # Adding abbreviations for convenience
         self.abbreviations = dict()
-        self.abbreviations['Comamonas testosteroni'] = 'ct'
-        self.abbreviations['Agrobacterium tumefaciens'] = 'at'
-        self.abbreviations['Microbacterium saperdae'] = 'ms'
-        self.abbreviations['Ochrobactrum anthropi'] = 'oa'
-        self.abbreviations['ct'] = 'Comamonas testosteroni'
-        self.abbreviations['at'] = 'Agrobacterium tumefaciens'
-        self.abbreviations['ms'] = 'Microbacterium saperdae'
-        self.abbreviations['oa'] = 'Ochrobactrum anthropi'
-      
-        strains = ['Agrobacterium tumefaciens','Comamonas testosteroni',\
-            'Microbacterium saperdae','Ochrobactrum anthropi']
-        self.strains = {k:list() for k in strains}
+        self.abbreviations["Comamonas testosteroni"] = "ct"
+        self.abbreviations["Agrobacterium tumefaciens"] = "at"
+        self.abbreviations["Microbacterium saperdae"] = "ms"
+        self.abbreviations["Ochrobactrum anthropi"] = "oa"
+        self.abbreviations["ct"] = "Comamonas testosteroni"
+        self.abbreviations["at"] = "Agrobacterium tumefaciens"
+        self.abbreviations["ms"] = "Microbacterium saperdae"
+        self.abbreviations["oa"] = "Ochrobactrum anthropi"
 
-        self.treatments = {'Agrobacterium tumefaciens':[1,3,4],
-            'Comamonas testosteroni':[2,3,4],
-            'Microbacterium saperdae':[3,4],
-            'Ochrobactrum anthropi':[4]}
+        strains = [
+            "Agrobacterium tumefaciens",
+            "Comamonas testosteroni",
+            "Microbacterium saperdae",
+            "Ochrobactrum anthropi",
+        ]
+        self.strains = {k: list() for k in strains}
 
-        work = '/work/FAC/FBM/DMF/smitri/evomicrocomm/genome_size/data'
-        self.references = {'Agrobacterium tumefaciens':join(work,'at','reference.fasta'),
-            'Comamonas testosteroni':join(work,'ct','reference.fasta'),
-            'Microbacterium saperdae':join(work,'ms','reference.fasta'),
-            'Ochrobactrum anthropi':join(work,'oa','reference.fasta')}
+        self.treatments = {
+            "Agrobacterium tumefaciens": [1, 3, 4],
+            "Comamonas testosteroni": [2, 3, 4],
+            "Microbacterium saperdae": [3, 4],
+            "Ochrobactrum anthropi": [4],
+        }
+
+        self.strains_per_treatment = {
+            1: ["Agrobacterium tumefaciens"],
+            2: ["Comamonas testosteroni"],
+            3: [
+                "Agrobacterium tumefaciens",
+                "Comamonas testosteroni",
+                "Microbacterium saperdae",
+            ],
+            4: [
+                "Agrobacterium tumefaciens",
+                "Comamonas testosteroni",
+                "Microbacterium saperdae",
+                "Ochrobactrum anthropi",
+            ],
+        }
+
+        work = "/work/FAC/FBM/DMF/smitri/evomicrocomm/genome_size/data"
+        self.references = {
+            "Agrobacterium tumefaciens": join(work, "at", "reference.fasta"),
+            "Comamonas testosteroni": join(work, "ct", "reference.fasta"),
+            "Microbacterium saperdae": join(work, "ms", "reference.fasta"),
+            "Ochrobactrum anthropi": join(work, "oa", "reference.fasta"),
+        }
 
         """For every strain and treatment we create a  list of dictionaries with
         the information about directory, sample name, treatment and sequencing platform"""
-        keys = ['dir_name','name','strain','platform','timepoint','treatment','cosm']
-        for i,row in self.df.iterrows():
-            meta = {key:None for key in keys}
-            meta['dir_name'] = row['dir_name']
-            meta['name'] = row['sample_name']
-            meta['strain'] = row['strain']
-            meta['platform'] = row['platform']
-            meta['timepoint'] = row['timepoint']
-            meta['treatment'] = int(row['treatment'])
-            meta['cosm'] = int(row['cosm'])
-            self.strains[row['strain']].append(meta)
+        keys = [
+            "dir_name",
+            "name",
+            "strain",
+            "platform",
+            "timepoint",
+            "treatment",
+            "cosm",
+        ]
+        for i, row in self.df.iterrows():
+            meta = {key: None for key in keys}
+            meta["dir_name"] = row["dir_name"]
+            meta["name"] = row["sample_name"]
+            meta["strain"] = row["strain"]
+            meta["platform"] = row["platform"]
+            meta["timepoint"] = row["timepoint"]
+            meta["treatment"] = int(row["treatment"])
+            meta["cosm"] = int(row["cosm"])
+            self.strains[row["strain"]].append(meta)
